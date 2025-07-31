1f4f7ce1ae0fe67ed5ca668215765b890f2caf76
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-07-31 22:17:58 +0530

    added the start game logic, choosing random btn, btn flash in level up

---
+++ script.js
@@ -0,0 +1,37 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow" , "red" , "green" , "purple"];
+
+let started = false;
+let level = 0;
+
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress", function(){
+    if(started == false){
+        started = true;
+
+        levelup();
+    }
+});
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function() {
+        btn.classList.remove("flash");
+    }, 1000);
+}
+
+function levelup(){
+    level++;
+    h2.innerText = `Level ${level}`; 
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randCol = btns[randIdx];
+    let randBtn = document.querySelector(`.${randCol}`);
+
+    //random btn choose
+    btnFlash(randBtn);
+}
