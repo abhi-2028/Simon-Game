01fe71d8e81e7fa69f58b5b823ebecffe9005786
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-08-01 16:18:47 +0530

    added the seq check logic, reset game logic and game over logic

--- script.js
+++ script.js
@@ -32,24 +32,51 @@ function userFlash(btn){
 }
 
 function levelup(){
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`; 
 
     let randIdx = Math.floor(Math.random() * 3);
     let randCol = btns[randIdx];
     let randBtn = document.querySelector(`.${randCol}`);
-
+    gameSeq.push(randCol);
     //random btn choose
     gameFlash(randBtn);
 }
 
+function checkSeq(idx){
+
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelup, 1000);
+        }
+    }else{
+        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press any key to start`;document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150);
+        reset();
+    }
+}
+ 
 function btnPress() {
     let btn = this;
     userFlash(btn);
-     
+    
+    userCol = btn.getAttribute("id");
+    userSeq.push(userCol);
+
+    checkSeq(userSeq.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns){
      btn.addEventListener("click", btnPress);
+}
+ 
+function reset(){
+    started = false;
+    userSeq = [];
+    gameSeq = [];
+    level = 0; 
 }
