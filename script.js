6a325ca0d9f5a7751a632404e53a929788950256
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-08-04 17:06:07 +0530

    added Start button

--- script.js
+++ script.js
@@ -6,24 +6,27 @@ let btns = ["yellow" , "red" , "green" , "purple"];
 let started = false;
 let level = 0;
 
-
 let h2 = document.querySelector("h2");
+let btnContainer = document.querySelector(".btn-container");
+let stBtn = document.querySelector(".st-btn");
 
-document.addEventListener("click", function(){
+
+document.querySelector(".st-btn").addEventListener("click", function(){
     if(started == false){
         started = true;
-
-        levelup();
+        btnContainer.classList.remove("before-start");
+        stBtn.classList.add("before-start");
+        setTimeout(levelup(), 9000);
     }
 });
 
-document.addEventListener("keypress", function(){
-    if(started == false){
-        started = true;
+// document.addEventListener("keypress", function(){
+//     if(started == false){
+//         started = true;
 
-        levelup();
-    }
-});
+//         levelup();
+//     }
+// });
 
 function gameFlash(btn){
     btn.classList.add("flash");
@@ -44,7 +47,7 @@ function levelup(){
     level++;
     h2.innerText = `Level ${level}`; 
 
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randCol = btns[randIdx];
     let randBtn = document.querySelector(`.${randCol}`);
     gameSeq.push(randCol);
@@ -59,7 +62,7 @@ function checkSeq(idx){
             setTimeout(levelup, 1000);
         }
     }else{
-        h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br>Press any key to start`;document.querySelector("body").style.backgroundColor = "red";
+        document.querySelector("body").style.backgroundColor = "red";
         setTimeout(function(){
             document.querySelector("body").style.backgroundColor = "white";
         },150);
@@ -83,6 +86,10 @@ for(btn of allBtns){
 }
  
 function reset(){
+    h2.innerHTML = `Game Over! Your score was <b>${level-1}</b> <br>Press any key to start`;
+    stBtn.innerText = "Restart";
+    stBtn.classList.remove("before-start");
+    btnContainer.classList.add("before-start");
     started = false;
     userSeq = [];
     gameSeq = [];
